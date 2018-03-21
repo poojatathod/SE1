028c933c7283a60c4d9ba32bea5fac816d614eeb
poojatathod <tathodpooja@gmail.com>
2018-03-21 17:26:31 +0530

    SE1 assign

---
+++ dictionary.py
@@ -0,0 +1,1 @@
+dict={"a":["ago","at"],"b":["bat","bag"],"c":["cat","cot","cage"],"d":["dog","dot","die","do"],"e":["ego","eat"],"g":["got","go","gcd"],"i":["it"],"o":["oat"],"t":["tie","tag","tab"]}
